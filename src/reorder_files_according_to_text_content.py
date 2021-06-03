44e1d1f6eac8378f98e326b8c6f9e8fbb87f5514
helnic <pont.daniel@gmail.com>
2021-06-03 10:16:05 +0200

    Find page containing the question and page containing the related answer

--- src/reorder_files_according_to_text_content.py
+++ src/reorder_files_according_to_text_content.py
@@ -12,6 +12,16 @@ text_content_file = "file_and_text.csv"
 
 def reorder_files():    
     print("Reorder files")
+    df_text_content = pd.read_csv(input_dir+"/"+text_content_file,header=0) 
+    df_text_content['order'] = -1
+    df_question = df_text_content[df_text_content['text'].str.contains('Q1:')]
+    df_answer = df_text_content[df_text_content['text'].str.contains('Q1: Correct Answer')]
+    df_question = df_question[df_question.isin(df_answer) == False]
+    print("Question :")
+    print(df_question.head(10))
+    print("Answer :")
+    print(df_answer.head(10))
+ 
 
 def main():
     # init output dirctory
@@ -20,8 +30,6 @@ def main():
     except OSError as e:
         print("Error: %s - %s." % (e.filename, e.strerror))
     os.mkdir(output_dir)
-    df_text_content = pd.read_csv(input_dir+"/"+text_content_file,header=1) 
-    print(df_text_content.head(10))
     reorder_files();
 
 if __name__ == '__main__':
